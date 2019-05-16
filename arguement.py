4546c0760d0f0e8f898e368d639e0effe21bf23a
彭美政 <923251674@qq.com>
2019-05-16 20:24:12 +0800

    v0.23
    目前用DBSCAN得到的最好的结果，参数分别为5.15和300，NMI为0.7793

--- arguement.py
+++ arguement.py
@@ -1,8 +1,4 @@
 import numpy as np
-<<<<<<< HEAD
-import matplotlib
-=======
->>>>>>> new
 import matplotlib.pyplot as plt
 
 
@@ -31,13 +27,6 @@ def get_distance_matrix(dataset):
     return np.array(dist_matrix)
 
 
-<<<<<<< HEAD
-def draw_scaatter_diagram(k_distancelist):
-    plt.figure()
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.scatter([i for i in range(len(k_distancelist))], np.sort(k_distancelist))
-=======
 def draw_scatter_diagram(datalist):
     sorted_datalist =np.sort(datalist)
     tmp = np.argsort(datalist)
@@ -47,19 +36,12 @@ def draw_scatter_diagram(datalist):
     plt.ylabel('Y')
     for i in range(len(datalist)):
         plt.scatter(i,sorted_datalist[i],c=colors[int(label[tmp[i]]/2-1)])
->>>>>>> new
     plt.show()
 
 
 # 观察k距离的矩阵得到k值
-<<<<<<< HEAD
-k = 120
-data, label = get_dataset()
-matrix = get_distance_matrix(data)
-draw_scaatter_diagram(matrix[:,k-1])
-=======
 k = 300
 data, label = get_dataset()
 matrix = get_distance_matrix(data)
 draw_scatter_diagram(matrix[:,k-1])
->>>>>>> new
+
